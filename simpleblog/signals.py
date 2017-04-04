6ba5e7740155ee430fc858eaf05124c88474ca8e
Emad Mokhtar <emad.m.habib@gmail.com>
2017-04-04 13:37:49 +0300

    Move simpleblog inside the project

--- simpleblog/signals.py
+++
@@ -1,8 +0,0 @@
-
-
-def save_comment(sender, instance, created, **kwargs):
-    if created:
-        comment = instance
-        post = comment.post
-        post.comment_count = post.comments.count()
-        post.save(force_update=True)
