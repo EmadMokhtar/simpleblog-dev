281ff763fa16cc69f3cd54d022171f093b0bf7fa
Emad Mokhtar <emad.m.habib@gmail.com>
2017-04-05 14:02:20 +0300

    Add tag to list blog posts categories

--- simpleblog_dev/simpleblog/templatetags/blog_tags.py
+++ simpleblog_dev/simpleblog/templatetags/blog_tags.py
@@ -1,6 +1,6 @@
 from django import template
 
-from ..models import Post
+from ..models import Post, PostCategory
 
 register = template.Library()
 
@@ -16,5 +16,18 @@ def latest_blog_posts(context, num):
         'latest_blog_posts': latest_blog_posts
     }
 
+
+def blog_categories(context):
+    """
+    Displays list of blog posts' categories
+    """
+    blog_categories = PostCategory.objects.all()
+
+    return {
+        'blog_categories': blog_categories
+    }
+
 register.inclusion_tag('tags/latest_blog_posts.html',
                        takes_context=True)(latest_blog_posts)
+register.inclusion_tag('tags/categories.html',
+                       takes_context=True)(blog_categories)
